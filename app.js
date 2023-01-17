c8b5a0b8e590eca503df462438f86a2a1c61cec9
nalberaDH <105679268+nalberaDH@users.noreply.github.com>
2023-01-16 21:31:49 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const port = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.listen(port, () => console.log(`servidor escuchando en puerto ${port}`));
